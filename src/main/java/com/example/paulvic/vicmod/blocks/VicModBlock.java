a4d2eb572c103563454bb0b02f8d24b646e45a2a
paulvic <paul_vickers@outlook.com>
2017-01-25 23:56:53 +0000

    Add texture to block
    Used paint.net to create a 16x16 png for texture

--- src/main/java/com/example/paulvic/vicmod/blocks/VicModBlock.java
+++ src/main/java/com/example/paulvic/vicmod/blocks/VicModBlock.java
@@ -1,6 +1,7 @@
 package com.example.paulvic.vicmod.blocks;
 
 import com.example.paulvic.vicmod.VicMod;
+import com.example.paulvic.vicmod.help.Reference;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -9,8 +10,11 @@ import net.minecraft.creativetab.CreativeTabs;
 public class VicModBlock extends Block {
 
 	public VicModBlock() {
-		super(Material.grass);
+		super(Material.glass);
 		this.setCreativeTab(VicMod.vicModTab);
+		this.setHardness(0.3f);
+		this.setLightLevel(50.0f);
+		this.setBlockTextureName(Reference.MODID + ":" + "vic_texture");
 	}
 }
 
