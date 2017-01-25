651af1f614c661fda359b4f948f3842c02542166
paulvic <paul_vickers@outlook.com>
2017-01-25 10:24:03 +0000

    Add new mod template and update .gitignore file

---
+++ src/main/java/com/example/paulvic/vicmod/VicMod.java
@@ -0,0 +1,22 @@
+package com.example.paulvic.vicmod;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+
+public class VicMod {
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@Mod.EventHandler
+	public void Init(FMLInitializationEvent event) {
+		
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+}
---
+++ src/main/java/com/example/paulvic/vicmod/help/Reference.java
@@ -0,0 +1,5 @@
+package com.example.paulvic.vicmod.help;
+
+public class Reference {
+
+}
